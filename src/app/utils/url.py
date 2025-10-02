c77d56922baa06010e7cf795d15c390488e92c5e
Petrișor Buga <petrisor.buga@qcatalyst.com>
2025-10-02 22:49:25 +0300

    fix: [app] `build_url` host always string

--- src/app/utils/url.py
+++ src/app/utils/url.py
@@ -2,6 +2,6 @@ from furl import furl
 
 
 def build_url(host, path="", query=None):
-    url = furl(host).add(path=path).add(args=(query or {})).url
+    url = furl(str(host)).add(path=path).add(args=(query or {})).url
 
     return url
